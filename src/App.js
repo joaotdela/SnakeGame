339515e90fdbb7a58bbdbd3950294ba9ca8f60d2
Joao T Dela <joaotdela@gmail.com>
2022-02-04 23:49:30 -0300

    I make the snake

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './index.css';
+import Snake from './Snake';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    snakeDots: [
+      [0, 0],
+      [2, 0]
+    ]
+  }
+  render() {
+    return (
+      <div className="game-area">
+        <Snake snakeDots={this.state.snakeDots} />
+      </div>
+    );
+  }
 }
 
 export default App;
